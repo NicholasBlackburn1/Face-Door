16040f155e20b62b939edfa187d907a9e67cb619
Nicholas Blackburn <nickblackburn02@gmail.com>
2020-10-26 19:58:50 -0400

    got smtp server working

--- src/cliet.py
+++ src/cliet.py
@@ -7,6 +7,8 @@ import zmq
 import time
 import smtplib
 import Config
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 context = zmq.Context()
 #  Socket to talk to server
@@ -16,36 +18,38 @@ client.setsockopt_string(zmq.SUBSCRIBE, "")
 client.connect("tcp://127.0.0.1:5000")
 print(client)
 
-server = smtplib.SMTP("smtp.gmail.com", 587)
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.ehlo()
 server.starttls()
-server.login(str(Config.SMSGATWAYEMAIL)), (str(Config.SMSGATEWAYPASSWORD))
-name = str(Config.NAME)
-nick = str(Config.NICKSPHONE)
-ethans = str(Config.ETHANSPHONE)
+server.login(Config.SMSGATWAYEMAIL, Config.SMSGATEWAYPASSWORD)
+name = Config.NAME
+nick = Config.NICKSPHONE
+ethans = Config.ETHANSPHONE
 
 while True:
    string = client.recv()
 
    if(string == b"starting"):
-       server.sendmail(name, nick, string +" " + "is Starting" + Config.ENDINGMESSAGE )
+       server.sendmail(name, nick, Config.NAME + " "+"Starting" + "  " + Config.ENDINGMESSAGE)
+       time.sleep(10)
      #  server.sendmail(name, ethans, string + " " + " is Starting" + Config.ENDINGMESSAGE )
         #  doorcontrol.doorOpen()
         #  doorcontrol.alarmOff()
-       time.sleep(10)
+       
    elif(string == b"owners"):
-       server.sendmail(name, nick, string +" " + " IS HERE! Opeing Door!"+ Config.ENDINGMESSAGE)
+       server.sendmail(name, nick, Config.NAME + " "+ "Nick or Ethan"+ "" +"is at the Studio" + "  " + Config.ENDINGMESSAGE )
        #server.sendmail(name, ethans, string + " " + " IS HERE! Opeing Door!" +Config.ENDINGMESSAGE)
         #  doorcontrol.doorOpen()
         #  doorcontrol.alarmOff()
        time.sleep(10)
    elif(string == b"parents"):
-       server.sendmail(name, nick, string +" " + " IS HERE! Opeing Door!"+ Config.ENDINGMESSAGE)
+       server.sendmail(name, nick, Config.NAME + " "+"Adults is at the Studio!"+ "  " +Config.ENDINGMESSAGE)
        #server.sendmail(name, ethans, string + " " + " IS HERE! Opeing Door!"+ Config.ENDINGMESSAGE)
         #  doorcontrol.doorOpen()
         #  doorcontrol.alarmOff()
        time.sleep(10)
    elif(string == b"unknown"):
-       server.sendmail(name,nick,string + " " +" IS HERE! Sounding Alarm!"+ Config.ENDINGMESSAGE)
+       server.sendmail(name,nick,Config.NAME + " " +" IS HERE! Sounding Alarm!"+ "  " +Config.ENDINGMESSAGE)
        #server.sendmail(name,ethans,string+ " " +" IS HERE! Sounding Alarm!"+ Config.ENDINGMESSAGE)
     #  doorcontrol.doorClose()
     #  doorcontrol.alarmOn()
@@ -53,6 +57,6 @@ while True:
    else:
     #  doorcontrol.doorClose()
      #  doorcontrol.alarmOff()
-       server.sendmail(name,nick,string + " " +"Error as occured "+ Config.ENDINGMESSAGE)
+       server.sendmail(name,nick,Config.NAME + " " +"Error as occured "+ "  " + Config.ENDINGMESSAGE)
        #server.sendmail(name,ethans,string+ " " +"Error as occured"+ Config.ENDINGMESSAGE)
        
