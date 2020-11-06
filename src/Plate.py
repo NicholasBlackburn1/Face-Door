b614967c4b8ada13b39443b9cd10a5a616d35936
Nicholas Blackburn <nickblackburn02@gmail.com>
2020-11-06 12:01:03 -0500

    adding plat rec

--- src/Plate.py
+++ src/Plate.py
@@ -19,7 +19,7 @@ from zmq.sugar import frame
 
     # TODO: Change this into the ipcamera Stream.
 video_capture = cv2.VideoCapture(0)
-video_capture.set(cv2.CAP_PROP_FPS, 15)
+video_capture.set(cv2.CAP_PROP_FPS, 60)
 
 
                     # Grab a single frame of video
@@ -27,26 +27,28 @@ video_capture.set(cv2.CAP_PROP_FPS, 15)
 
 while True:
     ret, frame = video_capture.read()    
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
+    img = cv2.resize(frame, (600,400) )
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
     gray = cv2.bilateralFilter(gray, 13, 15, 15) 
+
     edged = cv2.Canny(gray, 30, 200) 
-    
     contours = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = imutils.grab_contours(contours)
     contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
-    contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
-    screenCnt = 0 
-
+    crt = 0
     for c in contours:
         
         peri = cv2.arcLength(c, True)
         approx = cv2.approxPolyDP(c, 0.018 * peri, True)
-        screenCnt = approx
-        cv2.drawContours(frame, [approx], -1, (0, 0, 255), 3)
-        
+    
+        if len(approx) == 4:
+            screenCnt = approx
+            cv2.drawContours(img, [screenCnt], -1, (0, 0, 255), 3)
+        crt = approx
     mask = np.zeros(gray.shape,np.uint8)
-    new_image = cv2.drawContours(mask,screenCnt,0,255,-1,)
-    new_image = cv2.bitwise_and(frame,frame,mask=mask)
+    new_image = cv2.drawContours(mask,[crt],0,255,-1,)
+    new_image = cv2.bitwise_and(img,img,mask=mask)
 
     (x, y) = np.where(mask == 255)
     (topx, topy) = (np.min(x), np.min(y))
@@ -54,14 +56,16 @@ while True:
     Cropped = gray[topx:bottomx+1, topy:bottomy+1]
 
     text = pytesseract.image_to_string(Cropped, config='--psm 11')
-
+    
     print("Detected license plate Number is:",text)
-    cv2.imshow('car',frame)
-    cv2.imshow('gray', gray)
-    cv2.imshow('canny', edged)
-    #cv2.imshow('output', img)
- # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
+    img = cv2.resize(img,(500,300))
+    Cropped = cv2.resize(Cropped,(400,200))
+    cv2.imshow('car',img)
+    cv2.imshow('Cropped',Cropped)
 
-   
+        #cv2.imshow('output', img)
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    
