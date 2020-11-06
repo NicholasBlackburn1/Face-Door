32996450a520eb6b5dbce5287d0e51dc5ea81f7a
Nicholas Blackburn <nickblackburn02@gmail.com>
2020-11-06 13:06:07 -0500

    added text reconition

--- src/Plate.py
+++ src/Plate.py
@@ -14,38 +14,33 @@ import logging
 from datetime import datetime
 
 from zmq.sugar import frame
-
-
-
-    # TODO: Change this into the ipcamera Stream.
+# TODO: Change this into the ipcamera Stream.
 video_capture = cv2.VideoCapture(0)
 video_capture.set(cv2.CAP_PROP_FPS, 60)
 
-
-                    # Grab a single frame of video
- 
-
 while True:
+    
     ret, frame = video_capture.read()    
     img = cv2.resize(frame, (600,400) )
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
-    gray = cv2.bilateralFilter(gray, 13, 15, 15) 
+    gray = cv2.bilateralFilter(gray, 14, 15, 15) 
 
     edged = cv2.Canny(gray, 30, 200) 
     contours = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = imutils.grab_contours(contours)
     contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
     crt = 0
+    
     for c in contours:
-        
         peri = cv2.arcLength(c, True)
         approx = cv2.approxPolyDP(c, 0.018 * peri, True)
     
         if len(approx) == 4:
             screenCnt = approx
-            cv2.drawContours(img, [screenCnt], -1, (0, 0, 255), 3)
+
         crt = approx
+    cv2.drawContours(img, [crt], -1, (0, 0, 255), 3)
     mask = np.zeros(gray.shape,np.uint8)
     new_image = cv2.drawContours(mask,[crt],0,255,-1,)
     new_image = cv2.bitwise_and(img,img,mask=mask)
@@ -55,13 +50,16 @@ while True:
     (bottomx, bottomy) = (np.max(x), np.max(y))
     Cropped = gray[topx:bottomx+1, topy:bottomy+1]
 
-    text = pytesseract.image_to_string(Cropped, config='--psm 11')
+    text = pytesseract.image_to_string(Cropped, lang='eng',sconfig='--psm 10')
     
     print("Detected license plate Number is:",text)
     img = cv2.resize(img,(500,300))
     Cropped = cv2.resize(Cropped,(400,200))
     cv2.imshow('car',img)
     cv2.imshow('Cropped',Cropped)
+    cv2.imshow('contors', edged)
+    cv2.imshow('mask', mask)
+    cv2.imshow('gray', gray)
 
         #cv2.imshow('output', img)
     # Hit 'q' on the keyboard to quit!
