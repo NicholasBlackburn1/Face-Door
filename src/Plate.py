1e54df601bec9c5e264318b53c11b5394b8eba73
Nicholas Blackburn <nickblackburn02@gmail.com>
2020-11-06 13:09:11 -0500

    added commenrs

--- src/Plate.py
+++ src/Plate.py
@@ -27,6 +27,7 @@ while True:
     gray = cv2.bilateralFilter(gray, 14, 15, 15) 
 
     edged = cv2.Canny(gray, 30, 200) 
+    
     contours = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = imutils.grab_contours(contours)
     contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
@@ -35,16 +36,18 @@ while True:
     for c in contours:
         peri = cv2.arcLength(c, True)
         approx = cv2.approxPolyDP(c, 0.018 * peri, True)
-    
         if len(approx) == 4:
             screenCnt = approx
-
         crt = approx
+        
+    # Draws Contors
     cv2.drawContours(img, [crt], -1, (0, 0, 255), 3)
     mask = np.zeros(gray.shape,np.uint8)
+    
     new_image = cv2.drawContours(mask,[crt],0,255,-1,)
     new_image = cv2.bitwise_and(img,img,mask=mask)
 
+    # Crazy Image maths for TExt Recondition
     (x, y) = np.where(mask == 255)
     (topx, topy) = (np.min(x), np.min(y))
     (bottomx, bottomy) = (np.max(x), np.max(y))
@@ -55,6 +58,9 @@ while True:
     print("Detected license plate Number is:",text)
     img = cv2.resize(img,(500,300))
     Cropped = cv2.resize(Cropped,(400,200))
+    
+    
+    # TODO: remove views in release
     cv2.imshow('car',img)
     cv2.imshow('Cropped',Cropped)
     cv2.imshow('contors', edged)
