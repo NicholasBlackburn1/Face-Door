7fe079fce0c65348df20788065f489d49eca9be6
NicholasBlackburn1 <nickblackburn02@gmail.com>
2021-03-26 15:02:23 -0400

    added email sms server code to send email on opencv startup

--- src/prosessing/video.py
+++ src/prosessing/video.py
@@ -37,7 +37,7 @@ import wget
 import pathlib
 from configparser import ConfigParser
 from PIL import Image
-
+import SmsHandler
 # TODOD: add All Config.py Settings that arnt python fiunctions to Database
 
 
@@ -265,7 +265,7 @@ class VideoProsessing(object):
         '''
 
     def ProcessVideo(self):
-
+        SmsHandler.sendMessageToClient(logging,4123891615,"hello... OpenCv Subsystem is Starting UwU")
         # sets rtsp vsr in python
         os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp"
 
