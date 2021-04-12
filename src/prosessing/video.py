7d66efd8b5bc0714a0f1bf41be17cbfe8034889e
Nicholas Blackburn <nickblackburn02@gmail.com>
2021-04-12 13:56:53 -0400

    working on getting knn implmented in the Program

--- src/prosessing/video.py
+++ src/prosessing/video.py
@@ -21,9 +21,7 @@ import json
 
 import cv2
 
-
 import face_recognition
-from face_recognition.face_recognition_cli import image_files_in_folder
 import numpy as np
 import os
 from datetime import datetime
@@ -43,8 +41,7 @@ import pathlib
 from configparser import ConfigParser
 from PIL import Image
 import SmsHandler
-from sklearn import neighbors
-import pickle
+import KnnClassifiyer
 # TODOD: add All Config.py Settings that arnt python fiunctions to Database
 
 
@@ -173,114 +170,26 @@ class VideoProsessing(object):
                          str(user_info['image'])+', this may take a while...')
             logging.info("output file" +
                          str(user_info['image']+"at" + filepath))
+
+        # this function will load and prepare face encodes  for
     
-    #Creates Training File Structure For Knn 
-    def createTrainingDir(self,trainingdir,datalist):
+    def loadFacesIntoKnnGen(self):
+
+        print("Registering Faces to Face Rec Subsystem Please wait (Will Take a long time)")
+        logging.warn(
+            "Registering Faces to Face Rec Subsystem Please wait (Will Take a long time)")
+
+        encoding_user_faces_array = []
+        # defines all known faces for the system and how many times the dlib will train it self with that image takes min 49 sec to train
         i = 0
-        if(trainingdir == None):
-            os.mkdir(trainingdir)
-            while True:
-                os.chdir(trainingdir+self.getUserNames(datalist))
-                i+=1
-                if(i == db.getAmountOfEntrys):
-                    logging.info("Done Creating User Dirs")
-                return
+        while True:
 
-    # this function will load and prepare face encodes  for knn model so i can efficiently load and run face rec
-    def knnfaceModeltrain(self,train_dir, model_save_path=None, n_neighbors=None, knn_algo='ball_tree', verbose=False):
-   
-            """
-            Trains a k-nearest neighbors classifier for face recognition.
-            :param train_dir: directory that contains a sub-directory for each known person, with its name.
-            (View in source code to see train_dir example tree structure)
-            Structure:
-                <train_dir>/
-                ├── <person1>/
-                │   ├── <somename1>.jpeg
-                │   ├── <somename2>.jpeg
-                │   ├── ...
-                ├── <person2>/
-                │   ├── <somename1>.jpeg
-                │   └── <somename2>.jpeg
-                └── ...
-            :param model_save_path: (optional) path to save model on disk
-            :param n_neighbors: (optional) number of neighbors to weigh in classification. Chosen automatically if not specified
-            :param knn_algo: (optional) underlying data structure to support knn.default is ball_tree
-            :param verbose: verbosity of training
-            :return: returns knn classifier that was trained on the given data.
-            """
-            X = []
-            y = []
+            VideoProsessing.imagePathusers+self.decodeJsonImageData((self.datalist())[i])
+            i += 1
 
-            # Loop through each person in the training set
-            for class_dir in os.listdir(train_dir):
-                if not os.path.isdir(os.path.join(train_dir, class_dir)):
-                    continue
-
-                # Loop through each training image for the current person
-                for img_path in image_files_in_folder(os.path.join(train_dir, class_dir)):
-                    image = face_recognition.load_image_file(img_path)
-                    face_bounding_boxes = face_recognition.face_locations(image)
-
-                    if len(face_bounding_boxes) != 1:
-                        # If there are no people (or too many people) in a training image, skip the image.
-                        if verbose:
-                            print("Image {} not suitable for training: {}".format(img_path, "Didn't find a face" if len(face_bounding_boxes) < 1 else "Found more than one face"))
-                    else:
-                        # Add face encoding for current image to the training set
-                        X.append(face_recognition.face_encodings(image, known_face_locations=face_bounding_boxes)[0])
-                        y.append(class_dir)
-
-            # Determine how many neighbors to use for weighting in the KNN classifier
-            if n_neighbors is None:
-                n_neighbors = int(round(math.sqrt(len(X))))
-                if verbose:
-                    print("Chose n_neighbors automatically:", n_neighbors)
-
-            # Create and train the KNN classifier
-            knn_clf = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors, algorithm=knn_algo, weights='distance')
-            knn_clf.fit(X, y)
-
-            # Save the trained KNN classifier
-            if model_save_path is not None:
-                with open(model_save_path, 'wb') as f:
-                    pickle.dump(knn_clf, f)
-
-            return knn_clf
-
-    def predictKnnFaces(X_frame, knn_clf=None, model_path=None, distance_threshold=0.5):
-        """
-        Recognizes faces in given image using a trained KNN classifier
-        :param X_frame: frame to do the prediction on.
-        :param knn_clf: (optional) a knn classifier object. if not specified, model_save_path must be specified.
-        :param model_path: (optional) path to a pickled knn classifier. if not specified, model_save_path must be knn_clf.
-        :param distance_threshold: (optional) distance threshold for face classification. the larger it is, the more chance
-            of mis-classifying an unknown person as a known one.
-        :return: a list of names and face locations for the recognized faces in the image: [(name, bounding box), ...].
-            For faces of unrecognized persons, the name 'unknown' will be returned.
-        """
-        if knn_clf is None and model_path is None:
-            raise Exception("Must supply knn classifier either thourgh knn_clf or model_path")
-
-        # Load a trained KNN model (if one was passed in)
-        if knn_clf is None:
-            with open(model_path, 'rb') as f:
-                knn_clf = pickle.load(f)
-
-        X_face_locations = face_recognition.face_locations(X_frame)
-
-        # If no faces are found in the image, return an empty result.
-        if len(X_face_locations) == 0:
-            return []
-
-        # Find encodings for faces in the test image
-        faces_encodings = face_recognition.face_encodings(X_frame, known_face_locations=X_face_locations)
-
-        # Use the KNN model to find the best matches for the test face
-        closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=1)
-        are_matches = [closest_distances[0][i][0] <= distance_threshold for i in range(len(X_face_locations))]
-        return [(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]
-                
+            if(i == db.getAmountOfEntrys()):
+                logging.warn("Finished Registering Faces With Face Lib")
+                return encoding_user_faces_array
 
     # Fully Downloades USer Images and Returns No data
     def downloadUserFaces(self, imagePath):
@@ -322,8 +231,6 @@ class VideoProsessing(object):
                 if(i == db.getAmountOfEntrys):
                     logging.info("finished checking status")
                     return
-
-                    
     
     # gets USer name from Json String 
     def getUserNames(self, datalist):
@@ -339,7 +246,7 @@ class VideoProsessing(object):
                 logging.info("Done decoding string sending name to other code...")
                 return decodedstring['name']
             i+=1
-
+            
             if(i == db.getAmountOfEntrys):
                 logging.info("Done decoding string sending name to other code returning to main code UWU...")
                 return
@@ -392,11 +299,9 @@ class VideoProsessing(object):
 
         # Downlaods all the Faces 
         self.downloadUserFaces(VideoProsessing.imagePathusers)
-        known_face_encodings = []
-
-        # gets known face encodings 
-        known_face_encodings = ( self.loadFacesIntoFacialReconition())
         
+        #Trains Knn
+        KnnClassifiyer.train()
     
         # Initialize some variables
         face_locations = []
@@ -429,41 +334,8 @@ class VideoProsessing(object):
                 print("Broaking Image Uwu It does not Exsit fix")
 
             # Resize frame of video to 1/4 size for faster face detection processing
-            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-            rgb_small_frame = small_frame[:,:,0]
-            # Only process every other frame of video to save time
-            if True:
-                # Find all the faces and face encodings in the current frame of video
-                face_locations = face_recognition.face_locations(
-                    rgb_small_frame, model="cnn")
-                face_encodings = face_recognition.face_encodings(
-                    rgb_small_frame, face_locations
-                )
-
-                
-                for face_encoding in face_encodings:
-                    # See if the face is a match for the known face(s)
-                    matches = face_recognition.compare_faces(
-                        0, face_encoding, tolerance=0.6932
-                    )
-                    name = opencvconfig['unreconizedPerson']
-
-                    # # If a match was found in known_face_encodings, just use the first one.
-                    # if True in matches:
-                    #     first_match_index = matches.index(True)
-                    #     name = known_face_names[first_match_index]
-
-                    # Or instead, use the known face with the smallest distance to the new face
-                    face_distance = face_recognition.face_distance(
-                        known_face_encodings, face_encoding
-                    )
-                    best_match_index = np.argmin(face_distance)
-                    if matches[best_match_index]:
-                        name = known_face_names[best_match_index]
-
-                    face_names.append(name)
-
-            process_this_frame = not process_this_frame
+            small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+            
 
             # Loads Status of people 
             status = self.getUserStatusandCheckStatus(self.datalist())
@@ -472,7 +344,7 @@ class VideoProsessing(object):
             This Section is Deticated to dealing with user Seperatation via the User Stats data tag
             """
             # Display the results
-            for (top, right, bottom, left), name in zip(face_locations, face_names):
+            for name,(top, right, bottom, left) in predictions):
 
                 # Scale back up face locations since the frame we detected in was scaled to 1/4 size
                 top *= 4
