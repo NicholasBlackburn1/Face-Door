98fcfcbcb1f525db129b00212308d38f11b3df41
Nicholas Blackburn <nickblackburn02@gmail.com>
2021-04-24 20:00:22 -0400

    added placeholders to data on main page

--- src/web/app/base/routes.py
+++ src/web/app/base/routes.py
@@ -190,6 +190,7 @@ def index():
     return render_template(
         "index.html",
         lifetime_people= "Not imp",
+        platestotal= "Not imp"
         cpuload= psutil.cpu_percent(),
         Ram=psutil.virtual_memory().percent,
         uptime=datetime.now().strftime("%H:%M:%S")
